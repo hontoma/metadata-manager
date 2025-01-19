7c458330f3d520ba6119a0f6e353daa5cefa9984
hontoma <102908178+hontoma@users.noreply.github.com>
2025-01-20 01:25:03 +0900

    gitignoreの記述を修正

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gui import ImageAnalyzerApp
 if __name__ == "__main__":
     # image_analyzer.pyが存在する場合はインポート
     try:
-        from image_analyzer import ImageAnalyzer
+        from image_analyzer import ImageAnalyzer # type: ignore
         analyzer = ImageAnalyzer()
 
     # 存在しない場合はスキップ
